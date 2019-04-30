ee6deef88346747e1a6bc7ef5ec6a9a1f8f65db4
Fabian Fabro <fabrofabian@gmail.com>
2019-04-30 04:38:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,8 +62,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each {|season, holiday|
-    puts "#{season.capitalize!}:"
-    puts "#{holiday.capitalize!}:".chomp
+    puts "#{season.capitalize}:"
+    "#{holiday.capitalize}:".chomp
   }
 end
 
