003f78c7d8602795b53b5dca1da79e971800eb83
Fabian Fabro <fabrofabian@gmail.com>
2019-04-30 05:20:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect {|season, holiday|
     holiday.collect {|holiday, supply|
-      supply.has_value?("BBQ")
+      supply.key("BBQ")
         holiday
 
     }
