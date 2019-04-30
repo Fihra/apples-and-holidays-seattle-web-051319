53f16269a51667f2794ffad9a4810d938c23a1a8
Fabian Fabro <fabrofabian@gmail.com>
2019-04-30 04:38:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each {|season, holiday|
     puts "#{season.capitalize!}:"
-    puts "#{holiday.capitalize!}:"
+    puts "#{holiday.capitalize!}:".chomp
   }
 end
 
