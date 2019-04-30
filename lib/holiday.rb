a6fafcde3ba357735d220364f14e3de582b24508
Fabian Fabro <fabrofabian@gmail.com>
2019-04-30 05:25:59 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -88,8 +88,8 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.collect {|season, holiday|
-    holiday.collect {|holiday, supply|
-      supply.include?("BBQ")
+    holiday.select {|holiday, supply|
+      supply == "BBQ"
         holiday
 
     }
